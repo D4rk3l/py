89baeb7a3dfdb5728a5f79bfa62d261049dc6846
D4rk3l <40344588+D4rk3l@users.noreply.github.com>
2018-07-15 14:38:16 -0700

    loss

--- loss.py
+++ loss.py
@@ -1,34 +1,125 @@
 import time
 import sys
-
+import random
 def printOut(m):
     l = list(m)
     for i in range(0,len(l)):
         print(l[i],end='',flush=True)
-        time.sleep(0.009)
+        time.sleep(0.01)
     print()
 
 
+
+
+
+def house():
+
+    global cuts
+    global ankle
+    global sprainedWrist
+    inHouse = 1
+    
+    printOut("\nYou step into the hallway, ready for the day!")
+    printOut("You need to go downstairs and make some food.")
+    time.sleep(1)
+    while inHouse == 1:
+        randomChance = random.randint(0,10)
+        printOut("\nHow will you get down?\nSteps    Railing    Window")
+        options = input()
+        if options.upper() == "STEPS":
+            printOut("You walk down the stairs.")        
+        elif options.upper() == "RAILING":
+            printOut("You slide down the railing. Why not have a little fun?")
+            time.sleep(0.8)
+            printOut("You've never done this before, and you don't know how.\n")
+            time.sleep(1)
+            if randomChance >= 5:
+                printOut("You slide down nicely, satisfied with your attempt.")
+                break
+            elif randomChance < 5:
+                printOut("You fall off of the railing and sprain your wrist.\n")
+                time.sleep(2)
+                printOut("Great job.\n")
+                time.sleep(1)
+                printOut("You walk to the kitchen, gritting your teeth to withstand the pain of your wrist.")
+                sprainedWrist = 1
+                break
+        elif options.upper() == "WINDOW":
+            printOut("You walk up to the window.\nDive    Climb")
+            options = input()
+            if options.upper() == "DIVE":
+                printOut("\nYou rear up, getting ready to dive...")
+                time.sleep(1.5)
+                if randomChance >= 6:
+                    printOut("\nYou ace it! You break through the window and end on a roll from the two story drop.")
+                    time.sleep(2.0)
+                    printOut("You look up at the broken window, realizing that it wasn't the best idea to do that.")
+                    time.sleep(1.5)
+                    printOut("Whatever.")
+                    time.sleep(0.7)
+                    printOut("You enter the house from the back door, because you always forget to lock it.")
+                    
+                elif randomChance < 6:
+                    printOut("\nYou fail the dive, ending up with a lot of cuts on your skin from the broken glass.")
+                    time.sleep(1.8)
+                    printOut("You writhe in pain on the ground for a bit, and then pick yourself up.")
+                    time.sleep(1.5)
+                    cuts = 1
+                    printOut("You enter the house from the back door, because you always forget to lock it.")
+                    
+            if options.upper() == "CLIMB":
+                printOut("\nYou walk up to the window, release the lock, and open it.")
+                time.sleep(0.8)
+                printOut("You start to climb out...")
+                time.sleep(1)
+                
+                if randomChance >= 4:
+                    printOut("You safely climb down.")
+                    time.sleep(1)
+                    printOut("You enter the house from the back door, because you always forget to lock it.")
+                    
+                if randomChance < 4:
+                    printOut("You slip and twist your ankle from the fall.")
+                    time.sleep(1.2)
+                    ankle = 1
+                    printOut("You hobble into the house from the back door, because you always forget to lock it.")
+                    
+                
+
+                            
+                
+
+
 def bedroom():    # Bedroom
+    global sleptIn
     sleptIn = 0
+    global inBedroom
     inBedroom = 1
+    global wearSuit
     wearSuit = 0
+    global wearPajamas
     wearPajamas = 0
+    global wearDenim
     wearDenim = 0
+    global wearNothing
     wearNothing = 0
-    windowOpen = 0
+    global windowOpen
+    wearNothing = 0
+    
     while inBedroom == 1:
-        time.sleep(2.5)
-        printOut("\nYour options are...\nCloset    Bed    Window    Door\n")
+        time.sleep(1.5)
+        printOut("\nYour options are...\nCloset    Bed    Window    Door")
         options = input()
 
 
     
         if options.upper() == "CLOSET":    # Bedroom Closet
-            options = input("\nYou go over to your walk-in closet. Should you get dressed?\nYes    No\n")
+            printOut("\nYou go over to your walk-in closet. Should you get dressed?\nYes    No")
+            options = input()
                 
             if options.upper() == "YES":
-                options = input("\nWhat should you wear?\nSuit    Pajamas    Denim\n")
+                printOut("\nWhat should you wear?\nSuit    Pajamas    Denim")
+                options = input()
                 
                 if options.upper() == "SUIT":
                     printOut("\nYou're feeling fancy, so you put on your favorite suit and tie.")
@@ -63,7 +154,8 @@ def bedroom():    # Bedroom
 
                   
         elif options.upper() == "BED":    # Bedroom Bed
-            options = input("\nYou go back to bed. Sleep?\nYes    No\n")
+            printOut("\nYou go back to bed. Sleep?\nYes    No")
+            options = input()
 
             if options.upper() == "YES":
 
@@ -101,14 +193,16 @@ def bedroom():    # Bedroom
 
                     
         elif options.upper() == "WINDOW":    # Bedroom Window
-            options = input("\nYou walk to the window. Open it?\nYes    No\n")
+            printOut("\nYou walk to the window. Open it?\nYes    No")
+            options = input()
             if options.upper() == "YES":
                 if windowOpen == 0:
                     printOut("You decide to let the cool breeze in.")
                     windowOpen = 1
 
                 elif windowOpen == 1:
-                    options = input("The window is already open! Close it?\nYes    No")
+                    printOut("The window is already open! Close it?\nYes    No")
+                    options = input()
                     
                     if options.upper() == "YES":
                         printOut("You close the window.")
@@ -128,14 +222,17 @@ def bedroom():    # Bedroom
 
                 
         elif options.upper() == "DOOR":    # Bedroom Door
-            options = input("Exit your room?\nYes    No\n")
+            printOut("\nExit your room?\nYes    No")
+            options = input()
             
             if options.upper() == "YES":
-                printOut("ERROR: GAME NOT CODED.")
-                exit(0)
+                break
                 
             elif options.upper().lower == "NO":
-                printOut("You take some more time to get ready for the day.")
+                printOut("\nYou take some more time to get ready for the day.")
+
+
 
 bedroom()
- 
+house()
+
